33334615b3f2a48b55762968641909042e44ceb1
manudana11 <manueldanapallardo@gmail.com>
2024-03-20 12:39:59 +0100

    terminado arrays, vamos a por objetos. lets goooo

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // alert('JavaScript funciona correctamente')
 
+// parte variables
+
 let variableSinValor;
 
 let booleano1 = true;
@@ -14,7 +16,7 @@ const miNombre = 'Manuel';
 
 const miNumeroFav = 11;
 
-console.log(`Mi nombre es ${miNombre} y mi numero favorito es el ${miNumeroFav}`)
+console.log(`Mi nombre es ${miNombre} y mi numero favorito es el ${miNumeroFav}`);
 
 let frase1 = 'Seré un crack en JavaScript al terminar el bootcamp';
 
@@ -34,4 +36,36 @@ let meGustaJugarAl = `Me gusta jugar al ${miDeporteFav} los domingos`;
 
 console.log('fraseGolf', meGustaJugarAl);
 
-console.log(pi.toFixed(2))
+console.log('pi redondeado', pi.toFixed(2));
+
+// parte arrays
+
+let arrayVacio = [];
+
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let arrayNumeros2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let arrayNumerosPares = [0, 2, 4, 6, 8];
+
+let arraNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
+
+let holaMundo = ['Hola', 'Mundo'];
+
+let loGuardoTodo = ['hola', 'que', 23, 42.33, 'tal'];
+
+console.log(loGuardoTodo)
+
+let arrayDeArray = [[726, 'nombre'], [225, 'apellido'], [298, 'direccion']];
+
+console.log(loGuardoTodo.length);
+
+loGuardoTodo.push('pascual');
+
+console.log(loGuardoTodo);
+
+console.log(loGuardoTodo[1]);
+
+loGuardoTodo.push('Euralio')
+
+console.log(loGuardoTodo);

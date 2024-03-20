01a04e7070c8871034c49f131ea549bad31dba5a
manudana11 <manueldanapallardo@gmail.com>
2024-03-20 12:08:30 +0100

    parte variables

---
+++ main.js
@@ -0,0 +1,37 @@
+// alert('JavaScript funciona correctamente')
+
+let variableSinValor;
+
+let booleano1 = true;
+let booleano2 = false;
+
+const pi = 3.1416;
+const tau = 2 * pi;
+
+console.log('tau',tau);
+
+const miNombre = 'Manuel';
+
+const miNumeroFav = 11;
+
+console.log(`Mi nombre es ${miNombre} y mi numero favorito es el ${miNumeroFav}`)
+
+let frase1 = 'Seré un crack en JavaScript al terminar el bootcamp';
+
+console.log(frase1.toUpperCase());
+
+let crack = 'Hola soy un crack';
+
+console.log(crack.slice(0, 4));
+
+let numeroFavEscrito = miNumeroFav.toString();
+
+console.log('numeroFavEscrito', typeof numeroFavEscrito);
+
+let miDeporteFav = 'Golf';
+
+let meGustaJugarAl = `Me gusta jugar al ${miDeporteFav} los domingos`;
+
+console.log('fraseGolf', meGustaJugarAl);
+
+console.log(pi.toFixed(2))
